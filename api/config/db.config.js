38cfa3e9d85e220a2c4c86a55e5890daf1694037
Vishal kushwah <101382436+kushwahvishal939@users.noreply.github.com>
2023-06-12 09:45:52 +0530

    Update db.config.js

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: 'localhost',
     USER: "root",
-    PASSWORD: "",
+    PASSWORD: "vishal1234",
     DB: "primeSolutionMarket",
     dialect: "mysql",
     logging: false,
