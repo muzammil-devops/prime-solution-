465f7caf0d3d541dbe6a458a5727b5b10d81b259
Vishal kushwah <101382436+kushwahvishal939@users.noreply.github.com>
2023-06-20 13:08:53 +0530

    Update config.js

--- api/config.js
+++ api/config.js
@@ -7,8 +7,8 @@ dotenv.config({
 
 
 module.exports = {
-    NODE_ENV : process.env.NODE_ENV || 'development',
-    HOST : process.env.HOST || '13.50.101.209',
+    NODE_ENV : process.env.NODE_ENV || 'production',
+    HOST : process.env.HOST || '13.51.162.199',
     PORT : process.env.PORT || 4000,
     SECRET: process.env.SECRET || 'AAAAA'
 }
